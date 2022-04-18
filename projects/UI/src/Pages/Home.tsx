01496e84e8af453e14410b7cd66474d69c9e06b9
Donny Roufs <iamchets@yahoo.com>
2022-04-18 13:34:36 +0200

    feat: add base layout and add create widget

--- projects/UI/src/Pages/Home.tsx
+++ projects/UI/src/Pages/Home.tsx
@@ -1,7 +1,16 @@
 import { useGetSubreddits } from "../Queries/getSubreddits"
+import { CreateSubredditWidget } from "./CreateSubredditWidget/CreateSubredditWidget"
+import s from "./Home.module.scss"
 
 export const Home = () => {
   const [subreddits] = useGetSubreddits()
 
-  return <pre>{JSON.stringify(subreddits, null, 2)}</pre>
+  return (
+    <div className={s.container}>
+      <div className={s.content}>this is content</div>
+      <aside className={s.sidebar}>
+        <CreateSubredditWidget />
+      </aside>
+    </div>
+  )
 }
